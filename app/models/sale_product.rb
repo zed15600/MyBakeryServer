5e7d7bba172bb1d21e9cd5549d0d16a6783e45db
Edison Zapata <zed15600@gmail.com>
2021-03-18 17:30:52 -0400

    Reduces product stock with sale

--- app/models/sale_product.rb
+++ app/models/sale_product.rb
@@ -1,12 +1,20 @@
 class SaleProduct < ApplicationRecord
-  before_save :set_value
-
   belongs_to :sale, optional: true
   belongs_to :product, autosave: true
 
   validates :product_id, :quantity, presence: true
 
+  before_create :reduce_stock_create
+  before_save :set_value
+
+  private
+
   def set_value
     self.value = product.price * quantity
+  end
+
+  def reduce_stock_create
+    product.stock -= quantity
+    product.save
   end
 end
